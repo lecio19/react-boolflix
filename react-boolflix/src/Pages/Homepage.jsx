fdd1a6a452a32bfd17880809b5003e1fe84137d2
lecio19 <alex.macri01@gmail.com>
2024-12-15 17:44:22 +0100

    correzioni e aggiunte homepage

--- react-boolflix/src/Pages/Homepage.jsx
+++ react-boolflix/src/Pages/Homepage.jsx
@@ -4,6 +4,7 @@ import axios from "axios";
 export default function HomePage() {
   const [film, setFilm] = useState("");
   const [results, setResults] = useState([]);
+  
   const handleSearch = () => {
     if (!film) return; 
     axios
@@ -31,7 +32,10 @@ export default function HomePage() {
         {results.length > 0 ? (
           results.map((result) => (
             <li key={result.id}>
-              {result.title}
+              Titolo: {result.title} <br />
+              Titolo Originale: {result.original_title} <br />
+              Lingua: {result.original_language} <br />
+              Voto: {result.vote_average.toFixed(1)}
             </li>
           ))
         ) : (
